afe8e61d97ceeb2253c1b4ec32aa34d369839bec
ENUSHA M <85446900+EnushaM@users.noreply.github.com>
2022-08-30 22:54:16 +0530

    Update Qgenerator.py

--- Barcode reader using Computer Vision/Qgenerator.py
+++ Barcode reader using Computer Vision/Qgenerator.py
@@ -1,6 +1,6 @@
 import pyqrcode 
 from pyqrcode import QRCode
-surl = "https://www.pantechsolutions.net/computer-vision-crash-course/"
+surl = "https://in.pinterest.com/"
 url = pyqrcode.create(surl)
 url.svg("test.svg", scale = 8)
 url.png('test.png', scale=8)
